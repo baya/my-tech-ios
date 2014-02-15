97f24a981ce53e0e5fa12492d9b33fe898c26b77
baya <kayak.jiang@gmail.com>
2014-02-15 23:10:40 +0800

    update

---
+++ ToDoList05/ToDoList05/GrdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GrdAppDelegate.h
+//  ToDoList05
+//
+//  Created by jiangguimin on 14-2-15.
+//  Copyright (c) 2014年 ground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GrdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
